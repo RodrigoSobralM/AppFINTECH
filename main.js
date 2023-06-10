b804025f5872c5d9e7e553c473c3127e9d372fdf
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-06-10 20:34:20 -0300

    Upload telas login, cadastro e home. Javascript adicionado dando movimento e alerts

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-// Mascara formato CPF
-const cpf = document.getElementById('cpf')
-    for(let i = 0; i<cpf.length; i++) {
-        cpf.addEventListener('input', function() {
-            let input = this.value;
-            let numericValue = input.replace(/\D/g, '');
-            let maskedValue = numericValue.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4');
-            this.value = maskedValue;
-        });
+// // Mascara formato CPF
+const cpfCnpj = document.querySelectorAll('#cpfCnpj');
 
+for (let i = 0; i < cpfCnpj.length; i++) {
+  cpfCnpj[i].addEventListener('input', function(event) {
+    event.preventDefault()
+    let input = this.value;
+    let numericValue = input.replace(/\D/g, '');
+    let maskedValue;
+
+    if (numericValue.length <= 11) {
+      // CPF
+      maskedValue = numericValue.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4');
+    } else {
+      // CNPJ
+      maskedValue = numericValue.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5');
     }
 
-// Movimento labels nos input 
+    this.value = maskedValue;
+  });
+}
+
+// // Movimento labels nos input 
 const inputFields = document.querySelectorAll('.input-field');
 const inputContainers = document.querySelectorAll('.forms-groups');
 
@@ -26,24 +36,62 @@ const inputContainers = document.querySelectorAll('.forms-groups');
     });
     }
 
-// Movimento entre telas
+// // Movimento entre telas
+const cadastrarLink = document.getElementById('cadastrar');
+const returnLoginLink = document.getElementById('returnLogin');
 const loginSection = document.getElementById('login');
-const cadastroSection = document.getElementById('cadastro')
-const cadastroLink = document.querySelector('#cadastrar');
-const login = document.getElementById("returnLogin")
+const cadastroSection = document.getElementById('cadastro');
+const homeSection = document.getElementById('home')
+const btnEntrar = document.getElementById("btnEntrar")
+const btnSair = document.getElementById('btnSair')
+const verificando = document.querySelectorAll('.verificar')
+const senha = document.getElementById('senha')
 
-
-    cadastroLink.addEventListener('click', (event) => {
+btnEntrar.addEventListener('click', (event) => {
     event.preventDefault();
+    for (let i = 0; i < verificando.length; i++) {
+        if (verificando[i].value === "") {
+            alert('Preencha os campos');
+            return;
+        }
+    }
+    const todosPreenchidos = Array.from(verificando).every(input => input.value.trim() !== "");
+    
+    if (todosPreenchidos) {
+        homeSection.style.display = 'flex';
+        loginSection.style.display = 'none';
+    } else {
+        alert('Preencha todos os campos');
+    }
+});
+    
+btnSair.addEventListener('click', (event) => {
+    senha.value = "";
+    event.preventDefault()
+    homeSection.style.display = 'none'
+    loginSection.style.display = 'flex'
+})
 
-    loginSection.style.display = 'none';
-    cadastroSection.style.display = 'block';
+cadastrarLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    cadastroSection.classList.remove('slide-out');
+    loginSection.classList.remove('slide-in');
+    loginSection.classList.add('slide-out');
+    setTimeout(() => {
+        loginSection.style.display = 'none';
+        cadastroSection.style.display = 'flex';
+        cadastroSection.classList.add('slide-in');
+    }, 500);
+});
 
-    });
-    login.addEventListener('click', (event) => {
-        event.preventDefault()
-
-        loginSection.style.display = 'flex'
-        cadastroSection.style.display = 'none'
-
-    })
+returnLoginLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    cadastroSection.classList.remove('slide-in');
+    loginSection.classList.remove('slide-out');
+    cadastroSection.classList.add('slide-out')
+    setTimeout(() => {
+        loginSection.style.display = 'flex';
+        cadastroSection.style.display = 'none';
+        loginSection.classList.add('slide-in')
+    }, 500);
+});
